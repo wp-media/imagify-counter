8875bbd59bfe0b55458bc251f8c6e47184feee8d
Sébastien <sebastien.decamme@gmail.com>
2016-01-14 16:53:59 +0100

    RAZ

--- app/app.js
+++ app/app.js
@@ -26,35 +26,23 @@ var server = http.createServer(function (req, res) {
    Setting up socket.io
    ========================================================================== */
 
-var
-	io           = require('socket.io').listen(server),
-	currentValue = 0;
+var io = require('socket.io').listen(server);
 
-redisSubscriber.get('total_optimized_images', function (err, value) {
+redisSubscriber.subscribe( settings.redis.channel );
 
-	console.log('Total Optimized:' + value);
-
-	redisSubscriber.subscribe( settings.redis.channel );
-
-	redisSubscriber.on('message', function (channel, message) {
-		io.emit( 'counter', message );
-	});
-
-	io.sockets.on('connection', function (socket) {
-
-		io.emit( 'counter', currentValue );
-
-		console.log( 'Client connected'.cyan );
-
-	});
-
-	/* ==========================================================================
-	   Start service
-	   ========================================================================== */
-
-	server.listen(settings.server.port, function () {
-		console.log( ('Imagify Counter is listening on port ' + settings.server.port).bold.green );
-	});
-
+redisSubscriber.on('message', function (channel, message) {
+	io.emit( 'counter', message );
 });
 
+io.sockets.on('connection', function (socket) {
+	console.log( 'Client connected'.cyan );
+});
+
+
+/* ==========================================================================
+   Start service
+   ========================================================================== */
+
+server.listen(settings.server.port, function () {
+	console.log( ('Imagify Counter is listening on port ' + settings.server.port).bold.green );
+});
